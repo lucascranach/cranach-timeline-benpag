6d2f27a00b94d11b9bbe7b43b1b3741ca8fc48cc
Pagelsdorf Benjamin <bpagelsdorf@outlook.de>
2019-11-21 15:23:29 +0100

    update gallery

--- prototype/sprint/src/components/gallery/Gallery.vue
+++ prototype/sprint/src/components/gallery/Gallery.vue
@@ -1,18 +1,26 @@
 <template>
   <v-row>
     <v-col>
-      <v-row v-for="(category, index) in categories" :key="`category-${index}`">
-        <v-col cols="12" >
-          <v-banner color="success">
-            <strong class="headline">{{ category.type }}</strong>
-            <template v-slot:actions>
-              <v-btn text color="deep-purple accent-4">
-                {{ category.color }}
-              </v-btn>
-            </template>
-          </v-banner>
+      <v-row v-for="(category, index) in mutatedCategories" :key="`category-${index}`">
+        <v-col cols="12" class="py-0" :style="`background-color: ${category.color}`">
+          <v-row align="center">
+            <v-col cols="6" class="pl-10 py-0">
+              <v-row justify="start">
+                <strong class="headline">{{ category.type }}</strong>
+              </v-row>
+            </v-col>
+            <v-col cols="6" class="pr-10 py.0">
+              <v-row justify="end">
+                <v-btn icon x-large @click="changeCategoryState(index)"
+                       :style="`color: ${category.color}`" style="background-color: white">
+                  <v-icon v-if="categoryStates[index]">fa-chevron-up</v-icon>
+                  <v-icon v-else>fa-chevron-down</v-icon>
+                </v-btn>
+              </v-row>
+            </v-col>
+          </v-row>
         </v-col>
-        <v-col v-for="(item, itemIndex) in getFour(category.data)"
+        <v-col v-for="(item, itemIndex) in category.data"
                :key="`span-${index}-${itemIndex}`" cols="3" >
             <v-card>
               <v-img :src="item.imageUrl" max-height="200px" position="center top" />
@@ -37,7 +45,7 @@
                 </v-list-item>
               </v-list>
               <v-card-text text-right>
-                <a href="#">Weiterlesen...</a>
+                <a :href="item.link" :style="`color: ${category.color}`">Weiterlesen...</a>
               </v-card-text>
             </v-card>
         </v-col>
@@ -50,9 +58,8 @@
 export default {
   name: 'Gallery',
   data: () => ({
-    showAll: false,
+    categoryStates: [false, false, false, false],
   }),
-
   props: {
     categories: {
       /*
@@ -83,15 +90,28 @@ export default {
       required: true,
     },
   },
+  computed: {
+    mutatedCategories: {
+      get() {
+        return this.categories.map((c, i) => {
+          const newCat = { ...c };
+          if (!this.categoryStates[i]) newCat.data = newCat.data.slice(0, 4);
+          return newCat;
+        });
+      },
+    },
+  },
   methods: {
     filter(date) {
       return data => new Date(data.startDate).getTime() < date.getTime();
     },
-    getFour(list) {
-      return this.showAll ? list : list.slice(0, 4);
+    changeCategoryState(i) {
+      this.$set(this.categoryStates, i, !this.categoryStates[i]);
     },
-    changeState() {
-      this.showAll = !this.showAll;
+  },
+  watch: {
+    categories: (val) => {
+      this.$set(this.categoryStates, val.map(() => false));
     },
   },
 };

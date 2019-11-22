cf100bfbae76944cd3165ff01305fbf4c60e184b
ddubbert <dennis.dubbert@smail.th-koeln.de>
2019-11-22 10:48:05 +0100

    header

--- prototype/sprint/src/App.vue
+++ prototype/sprint/src/App.vue
@@ -1,37 +1,14 @@
 <template>
   <v-app>
-    <!-- <v-toolbar color="primary" dark style="height:10%">
-      <div class="d-flex align-center">
-        <router-link to="/">
-          <v-img
-            alt="Vuetify Logo"
-            class="shrink mr-2"
-            contain
-            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-            transition="scale-transition"
-            width="40"
-          />
-        </router-link>
-
-        <router-link to="/">
-          <v-img
-            alt="Vuetify Name"
-            class="shrink mt-1 hidden-sm-and-down"
-            contain
-            min-width="100"
-            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-            width="100"
-          />
-        </router-link>
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-toolbar-items>
-        <v-btn text to="variante1">Variante 1</v-btn>
-        <v-btn text to="variante2">Variante 2</v-btn>
-      </v-toolbar-items>
-    </v-toolbar> -->
+    <v-app-bar color="transparent" elevation="1">
+      <v-img
+        alt="Chranach Logo"
+        class="shrink mr-2"
+        contain
+        src="https://github.com/BenPag/cranach/blob/master/wiki/imgs/Cranach%20Timeline.png?raw=true"
+        height="50"
+      />
+    </v-app-bar>
 
     <v-content>
       <router-view/>

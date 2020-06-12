af5096a43a8eaca43b63118a5543eda90b77558f
Dominik Deimel <domi.deimel@yahoo.de>
2020-06-12 12:57:16 +0200

    colors for dark theme added

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -4,4 +4,20 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      dark: {
+        primary: '#444444',
+        secondary: '#222222',
+        accent: '#FECB2F',
+        lighten: '#FFFFFF',
+        lighten1: '#F2F2F2',
+        lighten2: '#F0F0F0',
+        lighten3: '#DDDDDD',
+        lighten4: '#B4B4B4',
+        dark: '#000321',
+      },
+    },
+    dark: true,
+  },
 });

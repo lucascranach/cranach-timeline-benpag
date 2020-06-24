6d7b7b795b567fa531d2e6689318ab80ef4569b4
BEP <bpagelsdorf@outlook.de>
2020-06-24 22:41:42 +0200

    add TimelineV3.vue as new approach

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,40 +5,65 @@ import works from '../../../backend/data/paintings.json';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-	state: {
-		items: [],
-		allItems: [],
-	},
-	mutations: {
-		setItems(state, items) {
-			state.items = items;
-		},
-		setAllItems(state, items) {
-			state.allItems = items;
-		},
-	},
-	actions: {
-		applyYearFilter({ commit, state }, { from, to }) {
-			const filteredItems = state.allItems.filter(
-				(i) => i.startDate >= from && i.endDate <= to,
-			);
-			console.log(state.allItems.length);
-			commit('setItems', filteredItems);
-		},
-		loadData({ commit }) {
-			const allItems = works.paintings.filter((w) => w.startDate > 1000);
-			commit('setItems', allItems);
-			commit('setAllItems', allItems);
-		},
-	},
-	modules: {
-	},
-	getters: {
-		getItems(state) {
-			return state.items;
-		},
-		getAllItems(state) {
-			return state.allItems;
-		},
-	},
+  strict: true,
+  devtools: true,
+  state: {
+    items: [],
+    allItems: [],
+    histogram: [],
+  },
+  mutations: {
+    setItems(state, items) {
+      state.items = items;
+    },
+    setAllItems(state, items) {
+      state.allItems = items;
+    },
+    calculateHistogram(state) {
+      state.histogram = state.allItems.reduce((histogram, item) => {
+        // reason: https://github.com/eslint/eslint/issues/8581
+        // eslint-disable-next-line no-param-reassign
+        histogram[item.startDate] = (histogram[item.startDate] || 0) + 1;
+        return histogram;
+      }, {});
+    },
+  },
+  actions: {
+    applyYearFilter({ commit, state }, { from, to }) {
+      const filteredItems = state.allItems.filter(
+        (i) => i.startDate >= from && i.endDate <= to,
+      );
+      commit('setItems', filteredItems);
+    },
+    loadData({ commit }) {
+      const allItems = works.paintings.filter((w) => w.startDate > 1000);
+      commit('setItems', allItems);
+      commit('setAllItems', allItems);
+      commit('calculateHistogram', allItems);
+    },
+  },
+  modules: {
+  },
+  getters: {
+    getItems(state) {
+      return state.items;
+    },
+    getAllItems(state) {
+      return state.allItems;
+    },
+    /*
+    getHistogramData(state) {
+      const histogram = {};
+      state.allItems.forEach((i) => {
+        if (histogram[i.startDate] !== undefined) {
+          histogram[i.startDate] += 1;
+        } else {
+          histogram[i.startDate] = 1;
+        }
+      });
+      console.log(histogram);
+      return histogram;
+    },
+    // */
+  },
 });

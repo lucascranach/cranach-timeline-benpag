6f8f60f6467c3fe196032bd52533c553f9fcaf3f
Pagelsdorf Benjamin <bpagelsdorf@outlook.de>
2020-06-25 11:04:07 +0200

    correct line endings

--- frontend/src/components/TimelineV2.vue
+++ frontend/src/components/TimelineV2.vue
@@ -15,100 +15,100 @@ import { mapGetters } from 'vuex';
 import d3 from '../plugins/d3-importer';
 
 export default {
-  name: 'ImageRangeSlider',
-  props: {
-  },
-  components: {},
-  data: function getData() {
-    return {
-      height: 100,
-      width: 1800,
-      x: null,
-      y: null,
-      svg: null,
-    };
-  },
-  computed: {
-    min() {
-      return Math.min(...this.years);
-    },
-    max() {
-      return Math.min(...this.years);
-    },
-    years() {
-      return Object.keys(this.getHistogramData());
-    },
-    displayWidth() {
-      if (this.minWidth > this.actualWidth) {
-        return this.minWidth;
-      }
-      return this.actualWidth;
-    },
-    displayHeight() {
-      if (this.minHeight > this.height) {
-        return this.minHeight;
-      }
-      return this.height;
-    },
-    actualWidth() {
-      if (this.width) {
-        return this.width;
-      }
-      if (document.getElementById(this.chartDivId)) {
-        return document.getElementById(this.chartDivId).clientWidth;
-      }
-      return null;
-    },
-  },
-  mounted() {
-    this.setupHistogramSvg();
-    this.setupAxis();
-    this.plotHistogram();
-  },
-  methods: {
-    ...mapGetters(['getHistogramData']),
-    setupHistogramSvg() {
-      this.svg = d3.select('#histogram')
-        .append('svg')
-        .attr('width', this.displayWidth)
-        .attr('height', this.displayHeight)
-        .append('g');
-    },
-    setupAxis() {
-      this.x = d3.scaleLinear()
-        .domain([this.min, this.max])
-        .range([0, this.width]);
-      this.y = d3.scaleLinear()
-        .domain([0, Math.max(...Object.values(this.getHistogramData()))])
-        .range([0, this.height]);
-    },
-    plotHistogram() {
-      const kde = this.kernelDensityEstimator(
-        this.kernelEpanechnikov(5), this.x.ticks(10),
-      );
-      const density = kde(Object.values(this.getHistogramData()));
+	name: 'ImageRangeSlider',
+	props: {
+	},
+	components: {},
+	data: function getData() {
+		return {
+			height: 100,
+			width: 1800,
+			x: null,
+			y: null,
+			svg: null,
+		};
+	},
+	computed: {
+		min() {
+			return Math.min(...this.years);
+		},
+		max() {
+			return Math.min(...this.years);
+		},
+		years() {
+			return Object.keys(this.getHistogramData());
+		},
+		displayWidth() {
+			if (this.minWidth > this.actualWidth) {
+				return this.minWidth;
+			}
+			return this.actualWidth;
+		},
+		displayHeight() {
+			if (this.minHeight > this.height) {
+				return this.minHeight;
+			}
+			return this.height;
+		},
+		actualWidth() {
+			if (this.width) {
+				return this.width;
+			}
+			if (document.getElementById(this.chartDivId)) {
+				return document.getElementById(this.chartDivId).clientWidth;
+			}
+			return null;
+		},
+	},
+	mounted() {
+		this.setupHistogramSvg();
+		this.setupAxis();
+		this.plotHistogram();
+	},
+	methods: {
+		...mapGetters(['getHistogramData']),
+		setupHistogramSvg() {
+			this.svg = d3.select('#histogram')
+				.append('svg')
+				.attr('width', this.displayWidth)
+				.attr('height', this.displayHeight)
+				.append('g');
+		},
+		setupAxis() {
+			this.x = d3.scaleLinear()
+				.domain([this.min, this.max])
+				.range([0, this.width]);
+			this.y = d3.scaleLinear()
+				.domain([0, Math.max(...Object.values(this.getHistogramData()))])
+				.range([0, this.height]);
+		},
+		plotHistogram() {
+			const kde = this.kernelDensityEstimator(
+				this.kernelEpanechnikov(5), this.x.ticks(10),
+			);
+			const density = kde(Object.values(this.getHistogramData()));
 
-      this.svg.append('path')
-        .attr('class', 'lime')
-        .datum(density)
-        .attr('fill', '#69b3a2')
-        .attr('opacity', '.8')
-        .attr('stroke', '#000')
-        .attr('stroke-width', 1)
-        .attr('stroke-linejoin', 'round')
-        .attr('d', d3.line()
-          .x((d) => this.x(d[0]))
-          .y((d) => this.y(d[1])).curve());
-    },
-    kernelDensityEstimator(kernel, X) {
-      return (V) => X.map((x) => [x, d3.mean(V, (v) => kernel(x - v))]);
-    },
-    kernelEpanechnikov(k) {
-      // eslint-disable-next-line max-len
-      // eslint-disable-next-line no-return-assign,no-mixed-operators,no-cond-assign,no-param-reassign
-      return (v) => (Math.abs(v /= k) <= 1 ? 0.75 * (1 - v * v) / k : 0);
-    },
-  },
+			this.svg.append('path')
+				.attr('class', 'lime')
+				.datum(density)
+				.attr('fill', '#69b3a2')
+				.attr('opacity', '.8')
+				.attr('stroke', '#000')
+				.attr('stroke-width', 1)
+				.attr('stroke-linejoin', 'round')
+				.attr('d', d3.line()
+					.x((d) => this.x(d[0]))
+					.y((d) => this.y(d[1])).curve());
+		},
+		kernelDensityEstimator(kernel, X) {
+			return (V) => X.map((x) => [x, d3.mean(V, (v) => kernel(x - v))]);
+		},
+		kernelEpanechnikov(k) {
+			// eslint-disable-next-line max-len
+			// eslint-disable-next-line no-return-assign,no-mixed-operators,no-cond-assign,no-param-reassign
+			return (v) => (Math.abs(v /= k) <= 1 ? 0.75 * (1 - v * v) / k : 0);
+		},
+	},
 };
 </script>
 
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,53 +5,53 @@ import works from '../../../backend/data/paintings.json';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  strict: true,
-  devtools: true,
-  state: {
-    items: [],
-    allItems: [],
-    histogram: [],
-  },
-  mutations: {
-    setItems(state, items) {
-      state.items = items;
-    },
-    setAllItems(state, items) {
-      state.allItems = items;
-    },
-    calculateHistogram(state) {
-      state.histogram = state.allItems.reduce((histogram, item) => {
-        // reason: https://github.com/eslint/eslint/issues/8581
-        // eslint-disable-next-line no-param-reassign
-        histogram[item.startDate] = (histogram[item.startDate] || 0) + 1;
-        return histogram;
-      }, {});
-    },
-  },
-  actions: {
-    applyYearFilter({ commit, state }, { from, to }) {
-      const filteredItems = state.allItems.filter(
-        (i) => i.startDate >= from && i.endDate <= to,
-      );
-      commit('setItems', filteredItems);
-    },
-    loadData({ commit }) {
-      const allItems = works.paintings.filter((w) => w.startDate > 1000);
-      commit('setItems', allItems);
-      commit('setAllItems', allItems);
-      commit('calculateHistogram', allItems);
-    },
-  },
-  modules: {
-  },
-  getters: {
-    getItems(state) {
-      return state.items;
-    },
-    getAllItems(state) {
-      return state.allItems;
-    },
-    /*
+	strict: true,
+	devtools: true,
+	state: {
+		items: [],
+		allItems: [],
+		histogram: [],
+	},
+	mutations: {
+		setItems(state, items) {
+			state.items = items;
+		},
+		setAllItems(state, items) {
+			state.allItems = items;
+		},
+		calculateHistogram(state) {
+			state.histogram = state.allItems.reduce((histogram, item) => {
+				// reason: https://github.com/eslint/eslint/issues/8581
+				// eslint-disable-next-line no-param-reassign
+				histogram[item.startDate] = (histogram[item.startDate] || 0) + 1;
+				return histogram;
+			}, {});
+		},
+	},
+	actions: {
+		applyYearFilter({ commit, state }, { from, to }) {
+			const filteredItems = state.allItems.filter(
+				(i) => i.startDate >= from && i.endDate <= to,
+			);
+			commit('setItems', filteredItems);
+		},
+		loadData({ commit }) {
+			const allItems = works.paintings.filter((w) => w.startDate > 1000);
+			commit('setItems', allItems);
+			commit('setAllItems', allItems);
+			commit('calculateHistogram', allItems);
+		},
+	},
+	modules: {
+	},
+	getters: {
+		getItems(state) {
+			return state.items;
+		},
+		getAllItems(state) {
+			return state.allItems;
+		},
+		/*
     getHistogramData(state) {
       const histogram = {};
       state.allItems.forEach((i) => {
@@ -65,5 +65,5 @@ export default new Vuex.Store({
       return histogram;
     },
     // */
-  },
+	},
 });

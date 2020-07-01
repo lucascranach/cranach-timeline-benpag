59bb15d9dcd9bdd0a3a1b8e7308332378335e1bb
Dominik Deimel <domi.deimel@yahoo.de>
2020-07-01 16:31:14 +0200

    Update EventTimeline.vue

--- frontend/src/components/EventTimeline.vue
+++ frontend/src/components/EventTimeline.vue
@@ -1,15 +1,16 @@
 <template>
 	<div>
 		<div id="timeline"></div>
-		<div id="toolTip">
-			<h1>{{toolTipData.title}}</h1>
-		</div>
+		<v-card id="toolTip" class="d3-tooltip">
+			<v-card-title>{{toolTipData.title}}</v-card-title>
+		</v-card>
 	</div>
 </template>
 
 <script>
 import * as d3 from 'd3';
 import lutherEvents from '@/assets/lutherEvents.json';
+import { event as currentEvent } from 'd3-selection';
 
 export default {
 	name: 'EventTimeline',
@@ -17,12 +18,15 @@ export default {
 		width: 1500,
 		height: 200,
 		toolTipData: {},
+		tooltipDiv: null,
 	}),
 	mounted() {
 		this.createEventTimeline();
 	},
 	methods: {
 		createEventTimeline() {
+			this.tooltipDiv = d3.select('#toolTip').style('visibility', 'hidden');
+
 			const svg = d3.select('#timeline')
 				.append('svg')
 				.attr('width', this.width)
@@ -34,38 +38,35 @@ export default {
 
 			svg.append('g')
 				.attr('transform', 'translate(0,50)') // This controls the vertical position of the Axis
-				.call(d3.axisBottom(x))
-				.tickFormat('')
-				.tickSize(0));
-
-			const toolTip = d3.select('#tooltip')
-				.append('div')
-				.style('opacity', 0)
-				.attr('class', 'tooltip')
-				.style('background-color', 'white')
-				.style('border', 'solid')
-				.style('border-width', '2px')
-				.style('border-radius', '5px')
-				.style('padding', '5px')
-				.style('position', 'absolute')
-				.style('overflow', 'hidden')
-				.style('z-index', 999999);
-
+				.call(d3.axisBottom(x)
+					.tickFormat('')
+					.tickSize(0));
 			lutherEvents.forEach((it) => {
 				const eventYear = new Date(it.x).getFullYear();
 				svg.append('circle')
 					.attr('cx', x(eventYear))
 					.attr('cy', 50)
-					.attr('r', 4)
+					.attr('r', 5)
 					.on('mouseover', () => {
 						this.toolTipData = it;
-						toolTip.style('opacity', 1);
-						toolTip.style('visibility', 'visible');
+						const tooltipHeight = this.tooltipDiv.node().getBoundingClientRect().height;
+						const sumHeight = tooltipHeight + currentEvent.y;
+						let top;
+						if (sumHeight < window.screen.height * 0.85) {
+							top = currentEvent.screenY;
+						} else if (currentEvent.y - tooltipHeight > 0) {
+							top = currentEvent.y - tooltipHeight;
+						} else {
+							top = currentEvent.y - tooltipHeight / 2;
+						}
+						this.tooltipDiv
+							.style('left', `${currentEvent.x}px`)
+							.style('top', `${top}px`)
+							.style('visibility', 'visible');
 					})
 					.on('mouseout', () => {
-						this.toolTipData = '';
-						toolTip.style('opacity', 0);
-						toolTip.style('visibility', 'hidden');
+						this.toolTipData = {};
+						this.tooltipDiv.style('visibility', 'hidden');
 					});
 			});
 		},
@@ -74,5 +75,13 @@ export default {
 </script>
 
 <style scoped>
-
+	.d3-tooltip {
+		position: absolute;
+		top: 0;
+		left: 0;
+		overflow: hidden;
+		text-align: center;
+		pointer-events: none;
+		z-index: 999999;
+	}
 </style>

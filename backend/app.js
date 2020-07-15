71952fd896be6701ce8099e8e233cdb389f4a33e
Alexander Strutz <alex-strutz@t-online.de>
2020-07-15 16:17:54 +0200

    Use routing in app (for obvious reasons)

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ const logger = require('morgan');
 const config = require('./global.config');
 const indexRouter = require('./routes/index');
 const paintingsRouter = require('./routes/paintings');
+const graphicsRouter = require('./routes/graphics');
+const archivalsRouter = require('./routes/archivals');
 
 const app = express();
 
@@ -17,6 +19,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/paintings', paintingsRouter);
+app.use('/graphics', graphicsRouter);
+app.use('/archivals', archivalsRouter);
 
 app.listen(config.port, () => {
 	console.log(`Server listen on port ${config.port}`);

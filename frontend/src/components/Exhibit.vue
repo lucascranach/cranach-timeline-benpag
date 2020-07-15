49eb168ce3fb564d7a34fd777d3d4f51d0ca81a9
AnleAnja <anleanja@live.de>
2020-07-15 16:25:44 +0200

    Create Exhibit.vue

---
+++ frontend/src/components/Exhibit.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <v-card
+            :elevation="hover ? 10 : 0"
+            class="pa-0"
+            :style="'border: 3px solid white'"
+            color="white"
+            width="300px">
+            <v-img
+                :src="'https://tr.rbxcdn.com/f95d9891bdebe2d001ac99f1af3cbf05/420/420/Decal/Png'"
+                position="center top"
+                class="pa-0"
+                :style="'background-color: white'"
+            />
+            <div class="headline">
+                Titel
+            </div>
+            <v-card-text class="pa-0">
+                <v-row>
+                    <v-col>
+                        <v-icon>mdi-flower</v-icon>
+                        <div>Blume</div>
+                    </v-col>
+                    <v-col>
+                        <v-icon>mdi-flower</v-icon>
+                        <div>Blume</div>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col>
+                        <v-icon>mdi-flower</v-icon>
+                        <div>Blume</div>
+                    </v-col>
+                    <v-col>
+                        <v-icon>mdi-flower</v-icon>
+                        <div>Blume</div>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col>
+                        <v-icon>mdi-flower</v-icon>
+                        <div>Blume</div>
+                    </v-col>
+                    <v-col>
+                        <v-icon>mdi-flower</v-icon>
+                        <div>Blume</div>
+                    </v-col>
+                </v-row>
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
+
+<script>
+export default {
+	name: 'Exhibit',
+};
+</script>
+
+<style scoped>
+
+</style>

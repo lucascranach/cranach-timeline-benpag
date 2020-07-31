ae16e24fa5bacf99f975d7dad1462936cdd11eec
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2020-07-31 11:40:55 +0200

    add frontend elements for filters

--- frontend/src/components/Filters.vue
+++ frontend/src/components/Filters.vue
@@ -1,29 +1,61 @@
 <template>
-    <sidebar-menu :menu="menu"/>
+    <v-row>
+        <v-col>
+            <v-btn @click="hideSearch = !hideSearch">Suche</v-btn>
+            <div v-if="!hideSearch">
+                <v-subheader>Volltext:</v-subheader>
+                <v-text-field></v-text-field>
+            </div>
+        </v-col>
+        <v-col>
+            <v-btn @click="hideCategory = !hideCategory">Kategorie</v-btn>
+            <v-list v-if="!hideCategory">
+                <v-list-item>
+                    <v-list-item-action>
+                        <v-switch></v-switch>
+                    </v-list-item-action>
+                    <v-list-item-title>Gemälde</v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                    <v-list-item-action>
+                        <v-switch></v-switch>
+                    </v-list-item-action>
+                    <v-list-item-title>Drucke</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-col>
+        <v-col>
+            <v-btn @click="hideTime = !hideTime">Zeit</v-btn>
+            <v-list v-if="!hideTime">
+                <v-list-item>
+                    <v-subheader>Von:</v-subheader>
+                    <v-text-field></v-text-field>
+                </v-list-item>
+                <v-list-item>
+                    <v-subheader>Bis:</v-subheader>
+                    <v-text-field></v-text-field>
+                </v-list-item>
+            </v-list>
+        </v-col>
+        <v-col>
+            <v-btn>Weitere</v-btn>
+        </v-col>
+        <v-col>
+            <v-btn>Reset</v-btn>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
 
-import Vue from 'vue';
-import VueSidebarMenu from 'vue-sidebar-menu';
-import 'vue-sidebar-menu/dist/vue-sidebar-menu.css';
-
-Vue.use(VueSidebarMenu);
 export default {
 	name: 'Filters',
-	computed: {
-		menu: [
-			{
-				href: '/',
-				title: 'Dashboard',
-				icon: 'fa fa-user',
-			},
-			{
-				href: '#',
-				title: 'Charts',
-				icon: 'fa fa-chart-area',
-			},
-		],
+	data() {
+		return {
+			hideSearch: true,
+			hideCategory: true,
+			hideTime: true,
+		};
 	},
 };
 </script>

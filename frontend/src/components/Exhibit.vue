154b69eab8daff30c5ee67828a5d08d807a6d312
Dominik Deimel <domi.deimel@yahoo.de>
2020-08-28 15:03:35 +0200

    fix indents

--- frontend/src/components/Exhibit.vue
+++ frontend/src/components/Exhibit.vue
@@ -105,20 +105,20 @@ export default {
     animation: kenburns 20s infinite;
 }
 @keyframes kenburns {
-0% {
-    opacity: 0.8;
-}
-5% {
-    opacity: 1;
-}
-95% {
-    transform: scale3d(1.5, 1.5, 1.5) translate3d(-50px, -120px, 0px);
-    animation-timing-function: ease-in;
-    opacity: 1;
-}
-100% {
-    transform: scale3d(2, 2, 2) translate3d(-170px, -100px, 0px);
-    opacity: 0;
-}
+    0% {
+        opacity: 0.8;
+    }
+    5% {
+        opacity: 1;
+    }
+    95% {
+        transform: scale3d(1.5, 1.5, 1.5) translate3d(-50px, -120px, 0px);
+        animation-timing-function: ease-in;
+        opacity: 1;
+    }
+    100% {
+        transform: scale3d(2, 2, 2) translate3d(-170px, -100px, 0px);
+        opacity: 0;
+    }
 }
 </style>

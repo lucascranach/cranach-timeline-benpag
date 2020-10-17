3d085ad08e3352ec96beeff2edf6a49b231ab4b1
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2020-10-17 20:43:02 +0200

    colors from colors.js instead of config

--- frontend/global.config.js
+++ frontend/global.config.js
@@ -1,11 +1,6 @@
 module.exports = {
 	dataBaseUrl: process.env.BACKEND_URL || 'http://localhost:3000/',
 	resources: ['paintings', 'graphics', 'archivals'],
-	colors: {
-		painting: '#c24749',
-		graphic: '#ffcc00',
-		archival: '#6b7ea8',
-	},
 	events: ['cranachElder', 'cranachYounger', 'luther', 'history'],
 	placeholderImageUrl: 'https://via.placeholder.com/350',
 	defaultDates: {

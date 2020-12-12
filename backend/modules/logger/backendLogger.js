4954436448f2d95b674780460b88bf6609b312c9
astrutz <--replace-all>
2020-12-12 22:26:35 +0100

    Remove external logger

--- backend/modules/logger/backendLogger.js
+++ backend/modules/logger/backendLogger.js
@@ -1,32 +1,3 @@
-const moment = require('moment-timezone');
-const { createLogger, format, transports } = require('winston');
-const config = require('../../global.config');
-
-const appendTimestamp = format((infoParam, opts) => {
-	const info = infoParam;
-	if (opts.tz) {
-		info.timestamp = moment().tz(opts.tz).format();
-	}
-	return info;
-});
-
-const backendLogger = createLogger({
-	level: config.logLevels.MinReqLevel,
-	format: format.combine(
-		format.errors({ stack: true }),
-		appendTimestamp({ tz: config.currentTimezone }),
-		format.prettyPrint(),
-	),
-	defaultMeta: { service: 'backend' },
-	transports: [
-		new transports.File({ filename: config.logFileNames.backend, level: config.logLevels.backendFile }),
-		new transports.Console({
-			level: config.logLevels.backendConsole,
-			handleExceptions: true,
-			json: false,
-			colorize: true,
-		}),
-	],
-});
+const backendLogger = console;
 
 module.exports = backendLogger;

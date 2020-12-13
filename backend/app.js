060b3a424736fa8a03e7559f0e65f560ec6b604e
BEP <bpagelsdorf@outlook.de>
2020-12-13 21:36:27 +0100

    add aws middleware

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path');
+const serverlessExpressMiddleware = require('aws-serverless-express/middleware');
 const cors = require('cors');
 
 const indexRouter = require('./routes/index');
@@ -14,14 +14,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(serverlessExpressMiddleware.eventContext());
 
 app.use('/', indexRouter);
-app.use((err, req, resp) => {
-	console.log(err);
-	console.log(req);
-	console.log(resp);
-});
 app.use('/paintings', paintingsRouter);
 app.use('/log', loggingRouter);
 app.use('/graphics', graphicsRouter);

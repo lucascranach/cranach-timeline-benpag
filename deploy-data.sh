117c2bdcd50599eb8f60d3dc3186f81103be9ff3
astrutz <--replace-all>
2020-12-21 22:49:02 +0100

    ..

--- deploy-data.sh
+++ deploy-data.sh
@@ -4,7 +4,7 @@ echo $bucketname
 
 if ! grep -q "$bucketname" <<< "$buckets"; then
  echo "Backend branch not there, new is one created"
- aws s3 mb s3://cranach-data-$bucketname
+ aws s3 mb s3://$bucketname
 fi
 
 aws s3 cp backend/data s3://$bucketname --recursive --acl public-read

660c5228a2a50300f91df0612e81a06a124933e3
astrutz <--replace-all>
2020-12-21 22:29:04 +0100

    ..

--- deploy-data.sh
+++ deploy-data.sh
@@ -5,12 +5,8 @@ echo $buckets
 echo $name
 #echo $element
 
-#case $BRANCH_NAME in
-case $name in
-
-  *"$buckets"*)
-    echo -n "It's there."
-    ;;
-esac
+if grep -q "$name" <<< "$buckets"; then
+ echo "It's there"
+fi
 
 #aws s3 cp backend/data s3://cranach-data-$BRANCH_NAME --recursive --acl public-read

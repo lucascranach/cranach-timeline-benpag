a6202c25400470d5a8b95be562bc6ade1756f4cf
astrutz <--replace-all>
2020-12-21 22:04:31 +0100

    ..

--- deploy-data.sh
+++ deploy-data.sh
@@ -1,11 +1,9 @@
 buckets=`aws s3 ls`
-element=`aws s3 ls s3://test`
+#element=`aws s3 ls s3://test`
 echo $buckets
-echo $element
-if [ $buckets == *$BRANCH_NAME* ]
-then
-echo "ja"
-
+#echo $element
+if [[ $buckets == *$BRANCH_NAME* ]]; then
+    echo "ja"
 fi
 
 #aws s3 cp backend/data s3://cranach-data-$BRANCH_NAME --recursive --acl public-read

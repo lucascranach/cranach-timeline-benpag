ea4b1e3ae7e5b56ae9ff1f3b14f12953e66b8419
astrutz <--replace-all>
2020-12-21 21:50:33 +0100

    ..

--- deploy-data.sh
+++ deploy-data.sh
@@ -1,1 +1,7 @@
-aws s3 cp backend/data s3://cranach-data-$BRANCH_NAME --recursive --acl public-read
+if [ aws s3 ls == *$BRANCH_NAME* ]
+then
+echo "ja"
+
+fi
+
+#aws s3 cp backend/data s3://cranach-data-$BRANCH_NAME --recursive --acl public-read

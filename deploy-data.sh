696a1cee0fcd369d3d89ffa0246a39dd030ee993
astrutz <--replace-all>
2020-12-21 22:37:34 +0100

    ..

--- deploy-data.sh
+++ deploy-data.sh
@@ -1,13 +1,13 @@
 
 buckets=`aws s3 ls`
-name='cranach-data'
+name='cranach-dataaa'
 #element=`aws s3 ls s3://test`
 echo $buckets
 echo $name
 #echo $element
 
-if grep -q "$name" <<< "$buckets"; then
- echo "It's there"
+if ! grep -q "$name" <<< "$buckets"; then
+ echo "It's not there"
 fi
 
 #aws s3 cp backend/data s3://cranach-data-$BRANCH_NAME --recursive --acl public-read

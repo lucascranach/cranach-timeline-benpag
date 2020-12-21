3fd6fbaa99406037d8a53f2edd6ecf104528ae1a
astrutz <--replace-all>
2020-12-22 00:24:21 +0100

    Last try on env

--- frontend/global.config.js
+++ frontend/global.config.js
@@ -2,7 +2,7 @@ function getBackendURL() {
 	if (process.env.VUE_APP_BRANCH_NAME) {
 		return process.env.VUE_APP_BACKEND_URL_HOST + process.env.VUE_APP_BRANCH_NAME.replace('/', '-') + process.env.VUE_APP_BACKEND_URL_PATH;
 	}
-	return process.env.VUE_APP_BACKEND_URL;
+	return process.env.BASE_URL;
 }
 
 module.exports = {

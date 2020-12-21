6fc6e3ce1922a00e3075089677f5153d773831ba
astrutz <--replace-all>
2020-12-21 22:22:43 +0100

    ..

--- deploy-data.sh
+++ deploy-data.sh
@@ -1,10 +1,11 @@
-#buckets=`aws s3 ls`
-buckets="cranach-data"
+buckets=`aws s3 ls`
+name='cranach-data'
 #element=`aws s3 ls s3://test`
 echo $buckets
 #echo $element
 
-case $BRANCH_NAME in
+#case $BRANCH_NAME in
+case $name in
 
   *"$buckets"*)
     echo -n "It's there."

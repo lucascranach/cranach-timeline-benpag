ae316928a2b5a902285d99e58f93c8e22acdd30a
BEP <bpagelsdorf@outlook.de>
2020-12-31 20:30:36 +0100

    improve tests add tests for store getters

---
+++ frontend/tests/unit/store/getters.spec.js
@@ -0,0 +1,82 @@
+import { expect } from 'chai';
+import state from '../../../src/store/state';
+import getters from '../../../src/store/getters';
+import testItemData from '../../testItemData.json';
+import config from '../../../global.config';
+// import testEventData from '../../testEventData.json';
+// import testFilters from '../../testFilters.json';
+
+describe('store getters', () => {
+	let testState;
+
+	beforeEach(() => {
+		testState = JSON.parse(JSON.stringify(state));
+		testState.allItems = testItemData;
+		testState.items = testItemData;
+	});
+
+	it('should get items', () => {
+		const result = getters.getItems(testState);
+		expect(JSON.stringify(testItemData)).to.be.equal(JSON.stringify(result));
+	});
+	it('should has items to be true', () => {
+		const result = getters.hasItems(testState);
+		// eslint-disable-next-line no-unused-expressions
+		expect(result).to.be.true;
+	});
+	it('should has items to be false because its empty', () => {
+		testState.items = [];
+		const result = getters.hasItems(testState);
+		// eslint-disable-next-line no-unused-expressions
+		expect(result).to.be.false;
+	});
+
+	it('should get all items', () => {
+		const result = getters.getAllItems(testState);
+		expect(JSON.stringify(testItemData)).to.be.equal(JSON.stringify(result));
+	});
+
+	it('should get images for histogram slider', () => {
+		const result = getters.getHistogramImages(testState);
+		const maxImageCount = testItemData.filter((i) => i.imageUrl).length;
+		expect(maxImageCount > 10 ? 10 : maxImageCount).to.be.equal(result.length);
+	});
+
+	it('should get dynamic x axis domain which is same as getStaticXAxisDomain until items are changed', () => {
+		const resultA = getters.getXAxisDomain(testState);
+		const resultB = getters.getStaticXAxisDomain(testState);
+		expect(JSON.stringify(resultA)).to.be.equal(JSON.stringify(resultB));
+	});
+	it('should get dynamic x axis domain which is different on changed items', () => {
+		const resultA = getters.getStaticXAxisDomain(testState);
+		testState.items.splice(0, testState.items.length / 4);
+		const resultB = getters.getXAxisDomain(testState);
+		expect(JSON.stringify(resultA)).to.be.not.equal(JSON.stringify(resultB));
+	});
+
+	it('should get static x axis domain with no default dates until allItems greater than 2', () => {
+		const resultA = getters.getStaticXAxisDomain(testState);
+		const resultB = [config.defaultDates.start, config.defaultDates.end];
+		expect(JSON.stringify(resultA)).to.be.not.equal(JSON.stringify(resultB));
+	});
+	it('should get static x axis domain with default dates when allItems are empty', () => {
+		testState.allItems = [];
+		const resultA = getters.getStaticXAxisDomain(testState);
+		const resultB = [config.defaultDates.start, config.defaultDates.end];
+		expect(JSON.stringify(resultA)).to.be.equal(JSON.stringify(resultB));
+	});
+	it('should get static x axis domain with same values even items are changed', () => {
+		const resultA = getters.getStaticXAxisDomain(testState);
+		testState.items = [];
+		const resultB = getters.getStaticXAxisDomain(testState);
+		expect(JSON.stringify(resultA)).to.be.equal(JSON.stringify(resultB));
+	});
+
+	it('should get distinct array of locations', () => {
+		const resultA = getters.getLocations(testState);
+		testState.items.concat(...testItemData);
+		const resultB = getters.getLocations(testState);
+
+		expect(JSON.stringify(resultA)).to.be.equal(JSON.stringify(resultB));
+	});
+});
--- frontend/tests/unit/store/mutations.spec.js
+++ frontend/tests/unit/store/mutations.spec.js
@@ -1,98 +1,84 @@
 import { expect } from 'chai';
 import { Transform } from 'd3-zoom/src/transform';
-import { zoomIdentity } from 'd3-zoom';
+import state from '../../../src/store/state';
 import mutations from '../../../src/store/mutations';
 import testItemData from '../../testItemData.json';
 import testEventData from '../../testEventData.json';
 import testFilters from '../../testFilters.json';
 
 describe('store mutations', () => {
-	let state;
+	let testState = {};
 
 	beforeEach(() => {
-		state = {
-			items: [],
-			allItems: [],
-			histogram: [],
-			chartHistogram: [],
-			events: {
-				cranachElder: [],
-				cranachYounger: [],
-				luther: [],
-				history: [],
-			},
-			activeFilters: [],
-			chartZoomTransform: zoomIdentity,
-			isLoading: false,
-		};
+		testState = JSON.parse(JSON.stringify(state));
 	});
 
 	it('should set items', () => {
-		mutations.setItems(state, testItemData);
-		expect(JSON.stringify(state.items)).to.be.equal(JSON.stringify(testItemData));
+		mutations.setItems(testState, testItemData);
+		expect(JSON.stringify(testState.items)).to.be.equal(JSON.stringify(testItemData));
 	});
 	it('should set all items', () => {
-		mutations.setAllItems(state, testItemData);
-		expect(JSON.stringify(state.allItems)).to.be.equal(JSON.stringify(testItemData));
+		mutations.setAllItems(testState, testItemData);
+		expect(JSON.stringify(testState.allItems)).to.be.equal(JSON.stringify(testItemData));
 	});
 
 	it('should set cranachElder events', () => {
 		const event = { cranachElder: testEventData };
-		mutations.setEvent(state, event);
-		expect(JSON.stringify(state.events.cranachElder)).to.be.equal(JSON.stringify(testEventData));
+		mutations.setEvent(testState, event);
+		expect(JSON.stringify(testState.events.cranachElder)).to.be.equal(JSON.stringify(testEventData));
 	});
 	it('should set cranachYounger events', () => {
 		const event = { cranachYounger: testEventData };
-		mutations.setEvent(state, event);
-		expect(JSON.stringify(state.events.cranachYounger)).to.be.equal(JSON.stringify(testEventData));
+		mutations.setEvent(testState, event);
+		expect(JSON.stringify(testState.events.cranachYounger)).to.be.equal(JSON.stringify(testEventData));
 	});
 	it('should set luther events', () => {
 		const event = { luther: testEventData };
-		mutations.setEvent(state, event);
-		expect(JSON.stringify(state.events.luther)).to.be.equal(JSON.stringify(testEventData));
+		mutations.setEvent(testState, event);
+		expect(JSON.stringify(testState.events.luther)).to.be.equal(JSON.stringify(testEventData));
 	});
 	it('should set history events', () => {
 		const event = { history: testEventData };
-		mutations.setEvent(state, event);
-		expect(JSON.stringify(state.events.history)).to.be.equal(JSON.stringify(testEventData));
+		mutations.setEvent(testState, event);
+		expect(JSON.stringify(testState.events.history)).to.be.equal(JSON.stringify(testEventData));
 	});
 
 	it('should set loading state', () => {
-		mutations.setLoadingState(state, true);
+		mutations.setLoadingState(testState, true);
 		// eslint-disable-next-line no-unused-expressions
-		expect(state.isLoading).to.be.true;
+		expect(testState.isLoading).to.be.true;
 	});
 
 	it('calculate histogram', () => {
-		mutations.setItems(state, testItemData);
-		mutations.calculateHistogram(state);
-		const sumOfItems = Object.values(state.histogram).reduce((sum, count) => sum + count, 0);
+		mutations.setItems(testState, testItemData);
+		mutations.calculateHistogram(testState);
+		const sumOfItems = Object.values(testState.histogram).reduce((sum, count) => sum + count, 0);
 		expect(sumOfItems).to.be.equal(testItemData.length);
 	});
 
 	it('should add filter', () => {
-		mutations.addFilter(state, testFilters[0]);
-		expect(JSON.stringify(state.activeFilters)).to.be.equal(JSON.stringify([testFilters[0]]));
+		mutations.addFilter(testState, testFilters[0]);
+		expect(JSON.stringify(testState.activeFilters)).to.be.equal(JSON.stringify([testFilters[0]]));
 	});
 	it('should remove filter', () => {
-		mutations.addFilter(state, testFilters[0]);
-		mutations.removeFilter(state, testFilters[0].name);
-		expect(state.activeFilters.length).to.be.equal(0);
+		mutations.addFilter(testState, testFilters[0]);
+		mutations.removeFilter(testState, testFilters[0].name);
+		expect(testState.activeFilters.length).to.be.equal(0);
 	});
 	it('should not remove filter', () => {
-		mutations.addFilter(state, testFilters[0]);
-		mutations.removeFilter(state, testFilters[1].name);
-		expect(state.activeFilters.length).to.be.equal(1);
+		mutations.addFilter(testState, testFilters[0]);
+		mutations.removeFilter(testState, testFilters[1].name);
+		expect(testState.activeFilters.length).to.be.equal(1);
 	});
 	it('should reset all filters', () => {
-		testFilters.forEach((f) => mutations.addFilter(state, f));
-		mutations.resetFilters(state);
-		expect(state.activeFilters.length).to.be.equal(0);
+		testFilters.forEach((f) => mutations.addFilter(testState, f));
+		mutations.resetFilters(testState);
+		expect(testState.activeFilters.length).to.be.equal(0);
 	});
 
 	it('should set the chart zoom transform', () => {
 		const newTransform = new Transform(2, 20, 20);
-		mutations.setChartZoomTransform(state, newTransform);
-		expect(state.chartZoomTransform).to.be.equal(newTransform);
+		mutations.setChartZoomTransform(testState, newTransform);
+		expect(testState.chartZoomTransform).to.be.equal(newTransform);
 	});
 });

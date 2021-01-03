6ece0232f27368f90e195324ed26ccfef57e0892
BEP <bpagelsdorf@outlook.de>
2021-01-03 18:16:01 +0100

    solve import data issues

--- deployment/import-data.sh
+++ deployment/import-data.sh
@@ -1,7 +1,13 @@
+branchname=${BRANCH_NAME////-}
+bucketname=data-${branchname,,}
+sevendaysago=`date --date="$1 day ago" +%s`
+buckets=`aws s3 ls`
+
 cd backend
 npm install
-sevendaysago=`date --date="$1 day ago" +%s`
 
-if aws s3api head-object --bucket data-feature-continuous-integration --key historyEvents_en.json --if-unmodified-since $sevendaysago
+if ! grep -q "$bucketname" <<< "$buckets"; then
+ then npm run import
+elif aws s3api head-object --bucket $bucketname --key historyEvents_en.json --if-unmodified-since $sevendaysago
   then npm run import
 fi
